eab4acac65f5f9744a59cf66a0e690b70556716c
abasalilov <abasalilov@gmail.com>
2019-09-24 20:04:51 -0700

    deployment build

---
+++ build/precache-manifest.f2e163406b626a2f3b8e31b6252e66ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c470b028af954ef752b05937a83a4141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fbdbce1770c5f5ea88a",
+    "url": "/static/css/main.1099afc1.chunk.css"
+  },
+  {
+    "revision": "78d42165fca233ecfbc8",
+    "url": "/static/js/2.623f6193.chunk.js"
+  },
+  {
+    "revision": "5fbdbce1770c5f5ea88a",
+    "url": "/static/js/main.f22b17fd.chunk.js"
+  },
+  {
+    "revision": "2787bac77704993fab4d",
+    "url": "/static/js/runtime-main.ed6d146d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
